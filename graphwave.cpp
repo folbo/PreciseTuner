ccdaf74d6c5094d0853a093a673d7b9083783c28
folbo <jacunia@op.pl>
2014-11-15 13:03:41 +0000

    Wave Graph and udpate

---
+++ graphwave.cpp
@@ -0,0 +1,5 @@
+#include "graphwave.h"
+
+graphwave::graphwave()
+{
+}
---
+++ graphwave.h
@@ -0,0 +1,10 @@
+#ifndef GRAPHWAVE_H
+#define GRAPHWAVE_H
+
+class graphwave
+{
+public:
+    graphwave();
+};
+
+#endif // GRAPHWAVE_H
